556a9f7c0e14159a4ac34fe73c2de08cc1b9c503
Dan Wilson <danwilson282@yahoo.co.uk>
2023-10-29 19:52:15 +0000

    content cache

---
+++ app/(site)/test/page.tsx
@@ -0,0 +1,18 @@
+// app/projects/[project]/page.tsx
+
+
+
+
+const Post = ({job=""}) => {
+  
+    return (
+      <article>
+        <h1>job</h1>
+      </article>
+    )
+  }
+  
+  
+
+  
+  export default Post
